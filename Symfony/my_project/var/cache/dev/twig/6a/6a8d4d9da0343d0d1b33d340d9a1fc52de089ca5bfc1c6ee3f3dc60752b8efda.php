fa49a1eacae0d1528bb43cfee76f314866738faf
yannsuty <yannsuty20@gmail.com>
2018-11-21 09:58:23 +0100

    css accueil

--- Symfony/my_project/var/cache/dev/twig/6a/6a8d4d9da0343d0d1b33d340d9a1fc52de089ca5bfc1c6ee3f3dc60752b8efda.php
+++ Symfony/my_project/var/cache/dev/twig/6a/6a8d4d9da0343d0d1b33d340d9a1fc52de089ca5bfc1c6ee3f3dc60752b8efda.php
@@ -39,25 +39,26 @@ class __TwigTemplate_849b6403b4620a2eac43fbd8bfb777882d09787020cc90b8da7466e7d8a
         // line 5
         $this->displayBlock('title', $context, $blocks);
         echo "</title>
-        ";
-        // line 6
-        $this->displayBlock('stylesheets', $context, $blocks);
-        // line 7
-        echo "            <link rel=\"stylesheet\" href=\"../assets/css/header.css\">
+            <link rel=\"stylesheet\" href=\"../assets/css/accueil.css\">
+            <link rel=\"stylesheet\" href=\"../assets/css/header.css\">
             <link rel=\"stylesheet\" href=\"https://www.w3school.com/w3css/4/w3.css\">
             <link rel=\"stylesheet\" href=\"https://fonts.googleapis.com/icon?family=Material+Icons\">
+        ";
+        // line 10
+        $this->displayBlock('stylesheets', $context, $blocks);
+        echo "    
     </head>
     <body>
         ";
-        // line 12
-        $this->displayBlock('header', $context, $blocks);
         // line 13
-        echo "        ";
-        $this->displayBlock('body', $context, $blocks);
+        $this->displayBlock('header', $context, $blocks);
         // line 14
         echo "        ";
-        $this->displayBlock('javascripts', $context, $blocks);
+        $this->displayBlock('body', $context, $blocks);
         // line 15
+        echo "        ";
+        $this->displayBlock('javascripts', $context, $blocks);
+        // line 16
         echo "    </body>
 </html>
 ";
@@ -87,7 +88,7 @@ class __TwigTemplate_849b6403b4620a2eac43fbd8bfb777882d09787020cc90b8da7466e7d8a
 
     }
 
-    // line 6
+    // line 10
     public function block_stylesheets($context, array $blocks = array())
     {
         $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
@@ -104,7 +105,7 @@ class __TwigTemplate_849b6403b4620a2eac43fbd8bfb777882d09787020cc90b8da7466e7d8a
 
     }
 
-    // line 12
+    // line 13
     public function block_header($context, array $blocks = array())
     {
         $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
@@ -121,7 +122,7 @@ class __TwigTemplate_849b6403b4620a2eac43fbd8bfb777882d09787020cc90b8da7466e7d8a
 
     }
 
-    // line 13
+    // line 14
     public function block_body($context, array $blocks = array())
     {
         $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
@@ -138,7 +139,7 @@ class __TwigTemplate_849b6403b4620a2eac43fbd8bfb777882d09787020cc90b8da7466e7d8a
 
     }
 
-    // line 14
+    // line 15
     public function block_javascripts($context, array $blocks = array())
     {
         $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
@@ -162,7 +163,7 @@ class __TwigTemplate_849b6403b4620a2eac43fbd8bfb777882d09787020cc90b8da7466e7d8a
 
     public function getDebugInfo()
     {
-        return array (  142 => 14,  125 => 13,  108 => 12,  91 => 6,  73 => 5,  61 => 15,  58 => 14,  55 => 13,  53 => 12,  46 => 7,  44 => 6,  40 => 5,  34 => 1,);
+        return array (  143 => 15,  126 => 14,  109 => 13,  92 => 10,  74 => 5,  62 => 16,  59 => 15,  56 => 14,  54 => 13,  48 => 10,  40 => 5,  34 => 1,);
     }
 
     public function getSourceContext()
@@ -172,10 +173,11 @@ class __TwigTemplate_849b6403b4620a2eac43fbd8bfb777882d09787020cc90b8da7466e7d8a
     <head>
         <meta charset=\"UTF-8\">
         <title>{% block title %}Welcome!{% endblock %}</title>
-        {% block stylesheets %}{% endblock %}
+            <link rel=\"stylesheet\" href=\"../assets/css/accueil.css\">
             <link rel=\"stylesheet\" href=\"../assets/css/header.css\">
             <link rel=\"stylesheet\" href=\"https://www.w3school.com/w3css/4/w3.css\">
             <link rel=\"stylesheet\" href=\"https://fonts.googleapis.com/icon?family=Material+Icons\">
+        {% block stylesheets %}{% endblock %}    
     </head>
     <body>
         {% block header %}{% endblock%}
