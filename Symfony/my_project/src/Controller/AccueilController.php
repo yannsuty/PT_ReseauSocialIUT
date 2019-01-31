ab8f2158c4bf5fdf04a5d179ce5c89b7f0ea0f00
VianneyDemarquet <vianney.demarquet@gmail.com>
2019-01-31 15:23:02 +0100

    connexion

--- Symfony/my_project/src/Controller/AccueilController.php
+++ Symfony/my_project/src/Controller/AccueilController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Form\Connexion;
@@ -15,21 +16,26 @@ class AccueilController extends AbstractController {
      * @Route("/", name="accueil")
      */
     public function index(Request $request, UserPasswordEncoderInterface $passwordEncoder) {
-        $user = new User();//Creation d'un user
-        $form = $this->createForm(Connexion::class, $user); //Creation du formulaire de connexion
-        $form->handleRequest($request); //On recupere les donnÃ©es du formulaires
-        if ($form->isSubmitted()) { //Si qq chose a Ã©tÃ© donnÃ©
+        //Creation d'un user
+        $user = new User();
+        //Creation du formulaire de connexion
+        $form = $this->createForm(Connexion::class, $user);
+        //On recupere les données du formulaires
+        $form->handleRequest($request);
+        //Si qq chose a été donné
+        if ($form->isSubmitted()) { 
             $em = $this->getDoctrine()->getManager(); //On charge doctrine pour des requetes sql
-            $connected = $em->getRepository(User::class)->findBy(["mail"=>$user->getMail()]); //On recupere le user par rapport au mail
-            if ($passwordEncoder->isPasswordValid($connected[0], $user->getPassword())) { //Si mdp est le bon
-            	//Creer session
-            	//Appeler function connecte
-            	echo "Bravo tu es connecte";
-            	return $this->render('accueil/connecte.html.twig');
+            $connected = $em->getRepository(User::class)->findBy(["mail"=>$user->getMail()])[0]; //On recupere le user par rapport au mail
+            if ($passwordEncoder->isPasswordValid($connected, $user->getPassword())) { //Si mdp est le bon
+                //Creer session
+                $session = new Session();
+                $session->set('user', $connected);
+                $nom = $connected->getPrenom()." ".$connected->getNom();
+            	return $this->render('accueil/connecte.html.twig',['connecte'=>true,"nom"=>$nom]);
             }
             echo "<br><br><br><br>";
 
         }
-        return $this->render('accueil/index.html.twig', ['form'=>$form->createView()]);
+        return $this->render('accueil/index.html.twig', ['form'=>$form->createView(),'connecte'=>false]);
  	}
 }
